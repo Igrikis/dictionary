2cc269c2ffdd14c5f8d430cea2c46374f5e4d7ca
igrik <igrik.is.parich@gmail.com>
2017-02-20 14:27:30 +0300

    deploy heroku

--- src/main/java/com/project/vocabulary/configuration/WebMvcConfiguration.java
+++ src/main/java/com/project/vocabulary/configuration/WebMvcConfiguration.java
@@ -1,6 +1,7 @@
 package com.project.vocabulary.configuration;
 
 import liquibase.integration.spring.SpringLiquibase;
+import org.apache.commons.dbcp.BasicDataSource;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,8 +11,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Created by Igor Ivanov
@@ -36,7 +39,7 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
         return new PropertySourcesPlaceholderConfigurer();
     }
 
-    @Bean
+    /*@Bean
     public DriverManagerDataSource getDataSource() {
         DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
         driverManagerDataSource.setDriverClassName(driverClassName);
@@ -44,10 +47,26 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
         driverManagerDataSource.setUsername(username);
         driverManagerDataSource.setPassword(password);
         return driverManagerDataSource;
+    }*/
+
+    @Bean
+    public BasicDataSource getDataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:mysql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
     }
 
     @Bean
-    public DataSourceTransactionManager transactionManager() {
+    public DataSourceTransactionManager transactionManager() throws URISyntaxException {
         return new DataSourceTransactionManager(getDataSource());
     }
 
@@ -59,7 +78,7 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
     }
 
     @Bean
-    public SpringLiquibase liquibase()  {
+    public SpringLiquibase liquibase() throws URISyntaxException {
         SpringLiquibase liquibase = new SpringLiquibase();
         liquibase.setDataSource(getDataSource());
         liquibase.setChangeLog("classpath:liquibase/db-changelog.xml");
